3aad099037cab6220db885b28aeaa40b30833e12
xesam <xesamguo@gmail.com>
2018-02-16 19:44:33 +0800

    'coll'

--- es6_dc.js
+++ es6_dc.js
@@ -1,3 +1,4 @@
+assert = require('assert');
 (function() {
   let opt = {
     name: "xesam",
@@ -48,23 +49,33 @@
     }
   };
 
-  let { info: { age, val='default' }, name } = opt;
+  let { info: { age, val = "default" }, name } = opt;
 
-//   console.log(info);//error
+  //   console.log(info);//error
   console.log(age);
   console.log(val);
   console.log(name);
 })();
 
-(function(){
-    let arr = ['a', 'b', 'c', 'd'];
-    let [a, b, , d, e='e'] = arr;
-    console.log(a, b, d, e);
+(function() {
+  let arr = ["a", "b", "c", "d"];
+  let [a, b, , d, e = "e"] = arr;
+  console.log(a, b, d, e);
 })();
 
-(function(){
-    let a = 1;
-    let b = 2;
-    [a, b] = [b, a];
-    console.log(a, b);
+(function() {
+  let a = 1;
+  let b = 2;
+  [a, b] = [b, a];
+  assert.equal(a, 2);
+  assert.equal(b, 1);
 })();
+
+//不定元素
+(function() {
+  let arr = [1, 2, 3, 4];
+  let [a, ...rest] = arr;
+  assert.equal(rest.length, 3);
+})();
+
+//结构参数应用默认值以后，变得更加冗长了，真是令人尴尬。
