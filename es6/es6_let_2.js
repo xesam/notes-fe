0eeeac2634d255c50d488b8c0328a7efb5934bdb
xesam <xesamguo@gmail.com>
2018-03-25 13:39:45 +0800

    let

--- es6/es6_let_2.js
+++ es6/es6_let_2.js
@@ -2,28 +2,32 @@ var assert = require("assert");
 
 (() => {
   var evs = [];
-  for (i = 0; i < 3; i++) {
-    var val = i; //this
-    evs.push(function() {
-      console.log(val);
+  var vals = [];
+  for (var i = 0; i < 3; i++) {
+    vals.push(i);
+    evs.push(function () {
+      return i;
     });
   }
 
-  evs.forEach(function(fn) {
-    fn();
+  evs.forEach(function (fn) {
+    var i = fn();
+    assert.notStrictEqual(vals.shift(), i); //notStrictEqual
   });
 })();
 
 (() => {
   var evs = [];
-  for (i = 0; i < 3; i++) {
-    let val = i; //this
-    evs.push(function() {
-      console.log(val);
+  var vals = [];
+  for (let i = 0; i < 3; i++) {
+    vals.push(i);
+    evs.push(function () {
+      return i;
     });
   }
 
-  evs.forEach(function(fn) {
-    fn();
+  evs.forEach(function (fn) {
+    var i = fn();
+    assert.strictEqual(vals.shift(), i); //strictEqual
   });
 })();
