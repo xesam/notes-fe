227a3a17b82da1c8441aa8de88c808ed33e326de
xesam <xesamguo@gmail.com>
2018-05-23 10:43:41 +0800

    vue

--- vue/vue-todos/src/App.vue
+++ vue/vue-todos/src/App.vue
@@ -1,25 +1,35 @@
 <template>
   <div id="app">
     <h1>{{title}}</h1>
-    <ul>
-        <li v-for="todo in todos" v-bind:key="todo.value">
-           <label>{{ todo.value }}</label>
+    <ul class="todos">
+        <li v-for="(todo, index) in todos" v-bind:key="todo.value" :class="{'checked': todo.done}">
+           <label>{{ index+1 }}.{{ todo.value }}</label>
+           <time>{{ todo.created | date }}</time>
         </li>
     </ul>
   </div>
 </template>
 
 <script>
+import "./assets/todos.less";
+import moment from "moment";
+import "moment/locale/zh-cn";
+moment.locale("zh-cn");
 export default {
   name: "app",
+  filters: {
+    date(val) {
+      return moment(val).calendar();
+    }
+  },
   data() {
     return {
       msg: "Vue todos",
       title: "Vue-todos",
       todos: [
-        { value: "阅读一本关于前端开发的书", done: false },
-        { value: "补充范例代码", done: true },
-        { value: "写心得", done: false }
+        { value: "阅读一本关于前端开发的书", done: false, created: Date.now() },
+        { value: "补充范例代码", done: true, created: Date.now() },
+        { value: "写心得", done: false, created: Date.now() }
       ]
     };
   }
