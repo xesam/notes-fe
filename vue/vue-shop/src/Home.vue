e78d89f752a98666d4d91f2f514de02109dcb4c7
xesam <xesamguo@gmail.com>
2018-05-30 19:54:07 +0800

    vue shop

--- vue/vue-shop/src/Home.vue
+++ vue/vue-shop/src/Home.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
-   首页
+   <book-list :books="books" @onBookClick="preview($event)"></book-list>
   </div>
 </template>
 
 <script>
-export default {};
+import Booklist from "./components/Booklist.vue";
+export default {
+  data() {
+    return {
+      books: [
+        {
+          title: "book 1",
+          id: "1"
+        },
+        {
+          title: "book 2",
+          id: "2"
+        },
+        {
+          title: "book 3",
+          id: "3"
+        }
+      ]
+    };
+  },
+  methods: {
+    preview(book) {
+      alert("click book " + book.title);
+    }
+  }
+};
 </script>
 
 <style>
---
+++ vue/vue-shop/src/components/Booklist.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="book-list">
+    <ul>
+    <li class="book" v-for="book in books" v-bind:key="book.id" @click="$emit('onBookClick', book)">
+        {{book.title}}
+    </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+let Booklist = {
+  props: ["books"],
+  filters: {
+    join(args) {
+      return args.json(",");
+    }
+  }
+};
+Vue.component("book-list", Booklist);
+export default Booklist;
+</script>
