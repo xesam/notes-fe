be5d74958896c5659e8d1672d3e5304ead3bf216
xesam <xesamguo@gmail.com>
2018-05-30 15:30:28 +0800

    vue shop

--- vue/vue-shop/src/App.vue
+++ vue/vue-shop/src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="tabs">
+        <ul>
+            <router-link tag="li" :to="{name:'home'}" >
+                <div>首页</div>
+            </router-link>
+            <router-link tag="li" :to="{name:'explorer'}" >
+                <div>分类</div>
+            </router-link>
+            <router-link tag="li" :to="{name:'cart'}">
+                <div>购物车</div>
+            </router-link>
+            <router-link tag="li" :to="{name:'me'}">
+                <div>我</div>
+            </router-link>
+        </ul>
+    </div>
+    <div class="content">
+        <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -31,31 +34,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- vue/vue-shop/src/main.js
+++ vue/vue-shop/src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
+import Home from './Home.vue'
+import Explorer from './Explorer.vue'
+import Cart from './Cart.vue'
+import Me from './Me.vue'
 
 Vue.use(VueRouter);
 
+const router = new VueRouter({
+    mode: 'history',
+    base: __dirname,
+    routes: [
+        { name: 'home', path: '/home', component: Home },
+        { name: 'explorer', path: '/explorer', component: Explorer },
+        { name: 'cart', path: '/cart', component: Cart },
+        { name: 'me', path: '/me', component: Me }
+    ],
+});
+
 new Vue({
     el: '#app',
+    router,
     render: h => h(App)
-})
+});
