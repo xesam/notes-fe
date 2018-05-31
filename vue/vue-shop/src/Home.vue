a19dcf5ed24dc97ec8cf2d681e61d562906d5fcb
xesam <xesamguo@gmail.com>
2018-05-31 16:11:36 +0800

    vue

--- vue/vue-shop/src/Home.vue
+++ vue/vue-shop/src/Home.vue
@@ -1,31 +1,58 @@
 <template>
-  <div>
-   <book-list :books="books" @onBookClick="preview($event)"></book-list>
-  </div>
+<div>
+<book-list :books="books" @onBookClick="preview($event)"></book-list>
+<modal-dialog ref="dialog"
+                @dialogClose="selected=undefined">
+        <div slot="header">
+            <div class="dismiss"
+                @click.prevent="$refs.dialog.close()"></div>
+        </div>
+        <div>
+            {{ selected ? selected.title : "" }}
+        </div>
+    </modal-dialog>
+</div>
 </template>
 
 <script>
-import Booklist from "./components/Booklist.vue";
 import Vue from "vue";
 import VueResource from "vue-resource";
+import Booklist from "./components/Booklist.vue";
+import ModalDialog from "./components/dialog.vue";
 Vue.use(VueResource);
 
 export default {
-  created() {
-    this.$http.get('/').then(data => {
-        console.log(data);
-    });
-  },
-  data() {
-    return {
-      books: []
-    };
-  },
-  methods: {
-    preview(book) {
-      alert("click book " + book.title);
+    created() {
+        this.$http.get("/").then(data => {
+            console.log(data);
+            this.$data.books = [
+                {
+                    title: "book 1",
+                    id: "1"
+                },
+                {
+                    title: "book 2",
+                    id: "2"
+                }
+            ];
+        });
+    },
+    data() {
+        return {
+            books: [],
+            selected: undefined
+        };
+    },
+    methods: {
+        preview(book) {
+            // alert("click book " + book.title);
+            this.selected = book;
+            this.$refs.dialog.open();
+        }
+    },
+    components: {
+        ModalDialog
     }
-  }
 };
 </script>
 
--- vue/vue-shop/src/router.js
+++ vue/vue-shop/src/router.js
@@ -1,37 +1,37 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-import Main from './Main.vue';
+import Main from "./Main.vue";
 
-import Home from './Home.vue'
-import Explorer from './Explorer.vue'
-import Cart from './Cart.vue'
-import Me from './Me.vue'
-import Book from './Book.vue'
+import Home from "./Home.vue";
+import Explorer from "./Explorer.vue";
+import Cart from "./Cart.vue";
+import Me from "./Me.vue";
+import Book from "./Book.vue";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-    mode: 'history',
-    base: __dirname,
-    routes: [
-        {
-            name: 'main',
-            path: '/',
-            component: Main,
-            children: [
-                { name: 'home', path: 'home', component: Home },
-                { name: 'explorer', path: 'explorer', component: Explorer },
-                { name: 'cart', path: 'cart', component: Cart },
-                { name: 'me', path: 'me', component: Me }
-            ]
-        },
-        {
-            name: 'book',
-            path: '/book/:id',
-            component: Book
-        }
-    ],
+  mode: "history",
+  base: __dirname,
+  routes: [
+    {
+      name: "main",
+      path: "/",
+      component: Main,
+      children: [
+        { name: "home", path: "home", component: Home },
+        { name: "explorer", path: "explorer", component: Explorer },
+        { name: "cart", path: "cart", component: Cart },
+        { name: "me", path: "me", component: Me }
+      ]
+    },
+    {
+      name: "book",
+      path: "/book/:id",
+      component: Book
+    }
+  ]
 });
 
-export default router
+export default router;
