4d73cea15cc1d623f7491d8736d6200756ce7084
xesam <xesamguo@gmail.com>
2018-06-27 10:45:02 +0800

    array

--- core/array-like.js
+++ core/array-like.js
@@ -1,9 +1,9 @@
 let assert = require("assert");
 
-let str_a = 'abcdefg';
+let str_a = "abcdefg";
 assert.equal(!!str_a.forEach, false);
 
-let b = [].slice(str_a, 0);
+let b = [].slice.call(str_a, 0);
 assert.equal(!!b.forEach, true);
 
 let c = Array.from(str_a);
