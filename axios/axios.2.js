c5476c1adc002c045b25b2cc545bb5e19a407e07
xesam <xesamguo@gmail.com>
2018-09-06 18:16:39 +0800

    axios

---
+++ axios/axios.2.js
@@ -0,0 +1,8 @@
+const axios_1 = require("axios");
+const axios_2 = require("axios").create();
+
+console.log(axios_1 === axios_2);
+
+// axios.get("https://xesam.github.io/").then(function (response) {
+//     console.log(response.status);
+// });
