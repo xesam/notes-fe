cf57359bfafb8e69c432763923beca958771439f
xesam <xesamguo@gmail.com>
2019-03-16 14:50:14 +0800

    del utils

--- utils/padding.js
+++
@@ -1,8 +0,0 @@
-function padding(number, length, prefix) {
-    if (String(number).length >= length) {
-        return String(number);
-    }
-    return padding(prefix + number, length, prefix);
-}
-console.log(padding(3, 5, "0"));
-console.log(padding(3, 5, "x"));
