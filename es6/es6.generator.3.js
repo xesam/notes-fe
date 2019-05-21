888177eeb7ff3184c1f937a96165095ed790bca1
xesam <xesamguo@gmail.com>
2019-05-21 11:29:31 +0800

    es6

--- es6/es6.generator.3.js
+++ es6/es6.generator.3.js
@@ -1,13 +1,13 @@
 // 迭代器传参的问题
 
-function* it() {
-    let r1 = yield 1;
+function* it(init) {
+    let r1 = yield 1 + init;
     let r2 = yield 2 + r1;
     let r3 = yield 3 + r2;
 }
 
-let i = it();
-console.log(i.next());
+let i = it(100);
+console.log(i.next(200));
 console.log(i.next(1));
 console.log(i.next(2));
 console.log(i.next(3));
